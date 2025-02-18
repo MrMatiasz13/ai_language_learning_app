d783f2bb6de67d843c89d987c8b624c7a8c7fa79
MrMatiasz13 <mateuszgrzeskow1328@gmail.com>
2025-02-18 20:40:07 +0100

    Added dependency injection

--- ai_language_learning_app/lib/dependency_injection.dart
+++ ai_language_learning_app/lib/dependency_injection.dart
@@ -1,4 +1,9 @@
 import 'package:ai_language_learning_app/features/chat/data/data_source/local/app_database.dart';
+import 'package:ai_language_learning_app/features/chat/data/repositories/database_repository_impl.dart';
+import 'package:ai_language_learning_app/features/chat/domain/repositories/database_repository.dart';
+import 'package:ai_language_learning_app/features/chat/domain/usecases/get_messages_usecase.dart';
+import 'package:ai_language_learning_app/features/chat/domain/usecases/send_message_usecase.dart';
+import 'package:ai_language_learning_app/features/chat/presentation/cubits/chat_cubit.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
@@ -8,4 +13,22 @@ Future<void> initializeDependencies() async {
   final database =
       await $FloorAppDatabase.databaseBuilder('app_database.db').build();
   sl.registerSingleton<AppDatabase>(database);
+
+  // repository
+  sl.registerSingleton<DatabaseRepository>(
+    DatabaseRepositoryImpl(sl()),
+  );
+
+  // usecase
+  sl.registerSingleton<GetMessagesUsecase>(
+    GetMessagesUsecase(sl()),
+  );
+  sl.registerSingleton<SendMessageUsecase>(
+    SendMessageUsecase(sl()),
+  );
+
+  // cubit
+  sl.registerFactory<ChatCubit>(
+    () => ChatCubit(sl(), sl()),
+  );
 }
